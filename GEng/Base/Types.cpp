37b7cf93d94fb9ed131217019c9cba746b4a8b65
deko89 <dects@yandex.ru>
2025-11-11 21:05:23 +0300

    Update order points

--- GEng/Base/Types.cpp
+++ GEng/Base/Types.cpp
@@ -72,8 +72,8 @@ void Points::MakeQuad(Val szX, Val szY)
 					};
 }
 void Points::MakeCube(Val s)
-{   *(Base*) this = {   {-s,-s,-s},  { s,-s,-s},  {s, s,-s},  {-s,s,-s},     // Низ.
-                        {-s,-s, s},  { s,-s, s},  {s, s, s},  {-s,s, s}   }; // Верх.
+{   *(Base*) this = {   {-s,-s,-s},  { s,-s,-s},  {-s, s,-s},  {s,s,-s},     // Низ.
+                        {-s,-s, s},  { s,-s, s},  {-s, s, s},  {s,s, s}   }; // Верх.
 }
 void Points::MakePlane(Val szX, Val szY, ValN sgmX, ValN sgmY)
 {	// Вычисление размера и выделение памяти.
@@ -193,9 +193,9 @@ void Mesh::MakeQuad(Val szX, Val szY)
 void Mesh::MakeCubeIn(Val s)
 {
     aVert.MakeCube(s);
-    aInd =  {   0,1,2, 0,2,3,  4,6,5, 4,7,6,    // Низ, верх.
-                3,2,7, 2,6,7,  0,4,1, 1,4,5,    // Вперёд, назад
-                0,3,4, 3,7,4,  1,6,2, 1,5,6,    // Лево, право.
+    aInd =  {   0,1,3, 0,3,2,  4,7,5, 4,6,7,    // Низ, верх.
+                2,3,6, 3,7,6,  0,4,1, 1,4,5,    // Вперёд, назад
+                0,2,4, 2,6,4,  1,7,3, 1,5,7,    // Лево, право.
             };
 }
 void Mesh::MakePlane(Val szX, Val szY, ValN sgmX, ValN sgmY)
