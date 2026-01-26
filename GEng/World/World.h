afd120133079aa4ee5984b42f106bb311cf83cd5
deko89 <dects@yandex.ru>
2026-01-26 16:55:32 +0300

    Selection

--- GEng/World/World.h
+++ GEng/World/World.h
@@ -11,11 +11,23 @@ namespace GEng
 using std::unique_ptr;
 using std::make_unique;
 
+/// Выделение.
+class Selection
+{	public:
+    vector<Model*> aMod;    ///< Выделенные модели.
+	/// Трансформация выделения.
+    Pos pos;
+	Angle angle;
+	Scale scale;
+    Mat4 mTransform;        ///< Матрица трансформации для выделенного.
+};
+
 /// Мир.
 class World
 {	public:
     unique_ptr<SkyBox> sky;     ///< Небо.
     Models models;              ///< Модели.
+    Selection sel;              ///< Выделенные модели.
 };
 
 }
