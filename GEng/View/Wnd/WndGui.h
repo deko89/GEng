39238efe43110b4b649881e6cc7d3eb0b20c8b6b
deko89 <dects@yandex.ru>
2026-03-16 18:23:08 +0300

    Delete demo window

--- GEng/View/Wnd/WndGui.h
+++ GEng/View/Wnd/WndGui.h
@@ -17,10 +17,6 @@ class WndGui : public WndG
 	void Draw(); ///< Рисование.
 protected:
 	ImGuiIO* pIO = nullptr;
-    // Our state
-    bool show_demo_window = false;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     void UpdateUI();
 };
 
